caea4480753dcc245779385cc029dff034e94dab
Dashamir Hoxha <dashohoxha@gmail.com>
2014-12-19 22:21:17 +0100

    Add a link to API reference for each example.

---
+++ api-examples-js/example_list.js
@@ -0,0 +1,88 @@
+// List of examples and their details.
+var example_list = {
+    report_statistics_get: {
+        jsfile: 'report/statistics_get.js',
+	apiref: '#sec-1.1',
+	jsbin: 'giweta/6/edit?js',
+    },
+    report_statistics_post: {
+	jsfile: 'report/statistics_post.js',
+	apiref: '#sec-1.1',
+	jsbin: null,
+    },
+    report_topcontrib_get: {
+	jsfile: 'report/topcontrib_get.js',
+	apiref: '#sec-1.2',
+	jsbin: null,
+    },
+    report_topcontrib_post: {
+	jsfile: 'report/topcontrib_post.js',
+	apiref: '#sec-1.2',
+	jsbin: null,
+    },
+    translations_get_random_sguid: {
+	jsfile: 'translations/get_random_sguid.js',
+	apiref: '#sec-2.1',
+	jsbin: null,
+    },
+    translations_retrieve: {
+	jsfile: 'translations/retrieve.js',
+	apiref: '#sec-2.2',
+	jsbin: null,
+    },
+    translations_get: {
+	jsfile: 'translations/get.js',
+	apiref: '#sec-2.2',
+	jsbin: null,
+    },
+    translations_list: {
+	jsfile: 'translations/list.js',
+	apiref: '#sec-2.7',
+	jsbin: null,
+    },
+    translations_search: {
+	jsfile: 'translations/search.js',
+	apiref: '#sec-2.7',
+	jsbin: null,
+    },
+    translations_add: {
+	jsfile: 'translations/add.js',
+	apiref: '#sec-2.3',
+	jsbin: null,
+    },
+    translations_vote: {
+	jsfile: 'translations/vote.js',
+	apiref: '#sec-2.4',
+	jsbin: null,
+    },
+    translations_submit: {
+	jsfile: 'translations/submit.js',
+	apiref: '#sec-2.8',
+	jsbin: null,
+    },
+    translations_import: {
+	jsfile: 'translations/import.js',
+	apiref: '#sec-2.9',
+	jsbin: null,
+    },
+    project_list: {
+	jsfile: 'project/list.js',
+	apiref: '#sec-3.1',
+	jsbin: null,
+    },
+    project_add_string: {
+	jsfile: 'project/add_string.js',
+	apiref: '#sec-3.4',
+	jsbin: null,
+    },
+    project_export: {
+	jsfile: 'project/export.js',
+	apiref: '#sec-3.2',
+	jsbin: null,
+    },
+    project_import: {
+	jsfile: 'project/import.js',
+	apiref: '#sec-3.6',
+	jsbin: null,
+    },
+};
--- api-examples-js/examples.js
+++ api-examples-js/examples.js
@@ -4,18 +4,41 @@ var debug = function(msg) {
 };
 
 $(document).ready(function() {
-    $('#examples' ).accordion({
+    $('#examples').accordion({
         active: 1,
         heightStyle: 'content',
     });
 
     // Load an example when it is clicked.
     $('.example').click(function(){
+        // Initialize HTML elements.
+        $('#jscode-title').html('Example Code');
         $('#jscode').html('');
         $('#output').html('');
 
-        var filename = this.id + '.js';
-        $.get(filename, 'script')
+        // Get the example details.
+	var id = this.id
+        if (example_list[id]) {
+	    var example = example_list[id];
+	}
+	else {
+	    alert("Example '" + id + "' not found!");
+	    return;
+	}
+
+        // Add button 'Try on JSBin'.
+	if (example.jsbin) {
+	    $('#jscode-title').append('<a href="http://jsbin.com/' + example.jsbin + '" target="_blank"><button class="btn">Try on JSBin</button></a>');
+	}
+
+        // Add button 'API Reference'.
+	if (example.apiref) {
+	    $('#jscode-title').append('<a href="http://info.btranslator.org/api/' + example.apiref + '" target="_blank"><button class="btn">API Reference</button></a>');
+	}
+
+        // Fetch and execute the JS file, 
+        // then highlight and display the code.
+        $.get(example.jsfile, 'script')
             .done(function (file_content) {
                 Rainbow.color(file_content, 'javascript',
 			      function(highlighted_code) {
