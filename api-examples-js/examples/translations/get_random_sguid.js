c02ef82d81a7acbda21f5aeb7ec282b81b77b0d8
Dashamir Hoxha <dashohoxha@gmail.com>
2015-06-04 23:46:21 +0200

    Remove last_sguid.

--- api-examples-js/examples/translations/get_random_sguid.js
+++ api-examples-js/examples/translations/get_random_sguid.js
@@ -7,18 +7,5 @@ var settings = {
 
 http_request(url, settings).done(function () {
     settings.data = { target: 'translated', lng: 'sq' };
-    http_request(url, settings).done(function () {
-        settings.data = {
-            target: 'translated',
-            lng: 'sq',
-            last_sguid: 'c7ac448a01c566680d8ffb00430a55ffc779f24b',
-        };
-        http_request(url, settings).done(function () {
-            settings.data = {
-                target: 'next',
-                last_sguid: 'c7ac448a01c566680d8ffb00430a55ffc779f24b',
-            };
-            http_request(url, settings);
-        });
-    });
+    http_request(url, settings);
 });
