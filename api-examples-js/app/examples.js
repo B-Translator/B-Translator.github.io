bd6d46a2ee944bd9ed54694ca7e35d6500a01b64
Dashamir Hoxha <dashohoxha@gmail.com>
2015-09-19 04:03:30 +0200

    Reorganize the JS code of the examples.

--- api-examples-js/app/examples.js
+++ api-examples-js/app/examples.js
@@ -1,63 +1,65 @@
 
+$(document).ready(function() {
+    // Build the HTML for the accordion by filling
+    // the template with the examples data.
+    var tmpl = $('#tmpl-examples').html();
+    var examples_html = Mustache.render(tmpl, example_data);
+    $('#examples').html(examples_html);
+
+    // Load an example when it is clicked.
+    $('.example').click(load_example);
+});
+
 // Write a debug message on the output area.
 var debug = function(msg) {
     $('#output').append(msg);
 };
 
-$(document).ready(function() {
-    // Build the HTML for the accordion by filling
-    // the template with the examples data.
-    var tmpl = $('#tmpl-examples').html();
-    $('#examples')
-        .html(Mustache.render(tmpl, example_data));
+var load_example = function(){
+    // Initialize HTML elements.
+    $('#jscode-title').text($(this).text());
+    $('#jscode').html('');
+    $('#output').html('');
 
-    // Load an example when it is clicked.
-    $('.example').click(function(){
-        // Initialize HTML elements.
-        $('#jscode-title').text($(this).text());
-        $('#jscode').html('');
-        $('#output').html('');
+    // Get the example details.
+    var jsfile = $(this).attr('jsfile');
+    if (!jsfile) {
+        alert("Example '" + jsfile + "' not found!");
+        return;
+    }
+    jsfile = 'examples/' + jsfile;
 
-        // Get the example details.
-        var jsfile = $(this).attr('jsfile');
-        if (!jsfile) {
-            alert("Example '" + jsfile + "' not found!");
-            return;
-        }
-        jsfile = 'examples/' + jsfile;
+    // Add button 'Try on JSBin'.
+    var jsbin = $(this).attr('jsbin');
+    if (jsbin) {
+        $('#jscode-title').append('<a href="http://jsbin.com/' + jsbin + '" target="_blank"><button class="btn btn-info btn-xs pull-right">Try on JSBin</button></a>');
+    }
 
-        // Add button 'Try on JSBin'.
-        var jsbin = $(this).attr('jsbin');
-        if (jsbin) {
-            $('#jscode-title').append('<a href="http://jsbin.com/' + jsbin + '" target="_blank"><button class="btn btn-info btn-xs pull-right">Try on JSBin</button></a>');
-        }
+    // Add button 'API Reference'.
+    var apiref = $(this).attr('apiref');
+    if (apiref) {
+        $('#jscode-title').append('<a href="http://info.btranslator.org/api/' + apiref + '" target="_blank"><button class="btn btn-info btn-xs pull-right">API Reference</button></a>');
+    }
 
-        // Add button 'API Reference'.
-        var apiref = $(this).attr('apiref');
-        if (apiref) {
-            $('#jscode-title').append('<a href="http://info.btranslator.org/api/' + apiref + '" target="_blank"><button class="btn btn-info btn-xs pull-right">API Reference</button></a>');
-        }
+    // Fetch the JS file then highlight and display the code.
+    $.ajax(jsfile, {dataType: 'text'})
+        .done(function (file_content) {
+            Rainbow.color(file_content, 'javascript',
+                          function(highlighted_code) {
+                              $('#jscode').html(highlighted_code);
+                          });
+        });
 
-        // Fetch the JS file then highlight and display the code.
+    // Wait 1sec, then fetch the JS file again and execute it.
+    setTimeout(function() {
         $.ajax(jsfile, {dataType: 'text'})
             .done(function (file_content) {
-                Rainbow.color(file_content, 'javascript',
-                              function(highlighted_code) {
-                                  $('#jscode').html(highlighted_code);
-                              });
+                $.globalEval(file_content);
             });
+    }, 1000)
 
-        // Wait 1sec, then fetch the JS file again and execute it.
-        setTimeout(function() {
-            $.ajax(jsfile, {dataType: 'text'})
-                .done(function (file_content) {
-                    $.globalEval(file_content);
-                });
-        }, 1000)
-
-        // Scroll to the section that tries the example.
-        $('html, body').animate({
-            scrollTop: $("#try").offset().top
-        }, 1000);
-    });
-});
+    // Scroll to the section that tries examples.
+    $('html, body').animate({
+        scrollTop: $("#try").offset().top
+    }, 1000);
+};
